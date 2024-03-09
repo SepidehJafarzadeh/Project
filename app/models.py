e2284be7ff03841f42264856fdc925d1d0d2ad41
SepidehJafarzadeh <sepidehjafarzadeh2004@gmail.com>
2024-03-09 15:51:24 +0330

    product was edited

--- app/models.py
+++ app/models.py
@@ -30,15 +30,24 @@ class User(AbstractUser):
         return self.email
 
 
+class Category(models.Model):
+    parent = models.ForeignKey('self', default=None, null=True, blank=True, on_delete=models.CASCADE, related_name='children')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(default=False)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(default=False)
     description = models.TextField(max_length=200)
     price = models.IntegerField()
     feature = models.ManyToManyField(ProductFeature)
-    category= models.ForeignKey("category", related_name="Product", on_delete=models.CASCADE, blank=True, null=True)
+    category= models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
     image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True, blank=True)
-
 
     def __str__(self):
         return self.name
@@ -52,15 +61,7 @@ class Comment(models.Model):
     def __str__(self):
         return self.product
     
-class Category(models.Model):
-    parent = models.ForeignKey('self', default=None, null=True, blank=True, on_delete=models.CASCADE, related_name='children')
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(default=False)
-    status = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.title
-    
 
 
 
